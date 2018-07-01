316ad3f57da580f16fde60efcdae8f0be86d1bd9
piokra <piotr.krasun@gmail.com>
2018-07-02 00:34:37 +0200

    Binaries

--- Assets/Scripts/CustomEventManager.cs
+++ Assets/Scripts/CustomEventManager.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using NUnit.Framework.Constraints;
-using UnityEditor.PackageManager;
-using UnityEditorInternal;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class CustomEventManager : MonoBehaviour
 {
